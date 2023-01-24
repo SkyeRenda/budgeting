097ac2e01cef5cd5c191b73e639289255d3af3fe
Skye Renda <skyerenda@hotmail.com>
2023-01-24 10:12:29 +0200

    preperation for backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {NavbarComponent} from './navbar/navbar.component';
 import { SummaryComponent } from './summary/summary.component';
 import { TransactionCardComponent } from './transaction-card/transaction-card.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import {NgApexchartsModule} from "ng-apexcharts";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  { path: '', component: SummaryComponent },
+];
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  providers: []
+})
+export class AppRoutingModule { }
+
 
 @NgModule({
   declarations: [
@@ -19,7 +30,8 @@ import {NgApexchartsModule} from "ng-apexcharts";
   ],
   imports: [
     BrowserModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
